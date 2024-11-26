db45a4bcd8dba6a26669e2844decc08d75e36693
napling <matthewcellis98@gmail.com>
2024-11-26 02:38:51 -0600

    working on fetch call

--- main/assets/js/script.js
+++ main/assets/js/script.js
@@ -1,16 +1,45 @@
+const APIKey = '4bf5cd1a7ad0fe45f259683b248d3892'
 const searchFormEl = document.querySelector('#search-form')
 
 function handleSearchFormSubmit(event) {
     event.preventDefault()
-
-    const searchInputVal = document.querySelector('city-search').value
+    const searchInputVal = document.querySelector('#city-search').value
 
     if (!searchInputVal) {
         console.error('please input a city name')
         return
     }
 
-    const queryString = ``
+    const citySearch = `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputVal}&limit=1&appid=${APIKey}`
+
+    fetch(citySearch)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json()
+            }
+            const json = response.json()
+            console.log(json)
+            return json
+        })
+        .then(function (data) {
+            console.log(data)
+            const lat = data[0].lat
+            const lon = data[0].lon
+            const weatherQuery = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`
+            console.log(lat, lon)
+            fetch(weatherQuery)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw response.json()
+                    }
+                    const json = response.json()
+                    console.log(json)
+                    return json
+                })
+                .then(function (data) {
+                    console.log(data)
+                })
+        })
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit)
